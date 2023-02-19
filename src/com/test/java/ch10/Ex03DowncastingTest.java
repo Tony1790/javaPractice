77a247eac24de521f681a4a44405908b4790cd14
Tony1790 <tony45081790@gmail.com>
2023-02-19 22:58:39 +0900

    Java practice at home

---
+++ src/com/test/java/ch10/Ex03DowncastingTest.java
@@ -0,0 +1,52 @@
+package com.test.java.ch10;
+
+import com.test.book.Book;
+
+public class Ex03DowncastingTest {
+
+    public static void main(String[] args) {
+        Book3 book = null;
+        EBook3 ebook = null;
+        ABook abook = null;
+
+        book = new Book3();
+        book.setTitle("자바 기본문법");
+        System.out.println(book.getTitle());
+
+        book = new EBook3();
+        book.setTitle("자바 알고리즘");
+        System.out.println(book.getTitle());
+        //book.setFontColor("블랙");
+        ebook = (EBook3)book;		// 다운캐스팅
+        ebook.setFontColor("블랙");
+        System.out.println(ebook.getFontColor());
+
+        book = new ABook();
+        book.setTitle("파이썬 알고리즘");
+        System.out.println(book.getTitle());
+        abook = (ABook)book;
+        abook.setVolume(50);
+        System.out.println(abook.getVolume());
+
+    }
+
+}
+
+class ABook extends Book3 {
+    private int volume;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
+
+/*
+문제 1.
+오디오북 클래스를 추가하세요.
+볼륨을 저장하는 정수타입의 속성을 추가하세요.
+main 메소드에서 다운캐스팅을 사용하여 오디오북의 볼륨을 출력하는 코드를 작성하세요.
+*/
---
+++ src/com/test/java/ch10/Ex04PolymorphismTest.java
@@ -0,0 +1,110 @@
+package com.test.java.ch10;
+
+import java.awt.event.WindowStateListener;
+
+public class Ex04PolymorphismTest {
+
+    public static void main(String[] args) {
+        Productz p = new Productz();
+        Productz p2 = p;
+        System.out.println(p.equalsProduct(p2));
+
+        Bookz b1 = new Bookz();
+        b1.setTitle("자바 자료구조");
+        b1.setAuthor("엘컴퓨터학원");
+        b1.setPrice(20000);
+
+        Bookz b2 = new Bookz();
+        b2.setTitle("C");
+        b2.setAuthor("엘컴퓨터학원");
+        b2.setPrice(10000);
+
+        Bookz b3 = new Bookz();
+        b3.setTitle("파이썬");
+        b3.setAuthor("엘코딩랩");
+        b3.setPrice(50000);
+
+        System.out.println(b1.equalsProduct(b2));
+        System.out.println(b1.equalsProduct(b3));
+
+        Bookz expensiveBook = (Bookz) b1.compareProduct(b2);
+        System.out.println("Title of the more expensive book: " + expensiveBook.getTitle());
+
+        expensiveBook = (Bookz) b1.compareProduct(b3);
+        System.out.println("Title of the more expensive book: " + expensiveBook.getTitle());
+
+    }
+
+}
+
+class Productz {
+    public boolean equalsProduct(Productz p) {
+        return this == p;
+    }
+
+    public Productz compareProduct(Productz p){
+        return null;
+    }
+
+
+}
+
+class Bookz extends Productz{
+    private String title;
+    private String author;
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equalsProduct(Productz p) {
+        if (!(p instanceof Bookz))
+            return false;
+        Bookz book = (Bookz)p;
+        boolean result = title.equals(book.title) && author.equals(book.author);
+
+        return result;
+    }
+
+    @Override
+    public Productz compareProduct(Productz p) {
+        if (!(p instanceof Bookz))
+            return null;
+        Bookz book = (Bookz) p;
+        if (this.getPrice() > book.getPrice()) {
+            return this;
+        } else {
+            return book;
+        }
+    }
+
+}
+
+/*
+문제 1.
+Productz 클래스에 두 인스턴스간의 크기를 비교하는 메소드인 compareProduct 메소드를 추가하세요.
+Bookz 클래스에 compareProduct 메소드를 오버라이딩 후 가격이 높은 책의 인스턴스를 반환하도록 코딩하세요.
+반환된 인스턴스의 title을 출력하세요.
+*/
