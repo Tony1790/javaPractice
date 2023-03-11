2dbe776621cc7a498fa7a28f04dc3a5be713514d
Tony1790 <tony45081790@gmail.com>
2023-03-12 00:13:52 +0900

    Java practice at home

--- src/com/test/java/ch18/Ex04QueueTest.java
+++ src/com/test/java/ch18/Ex04QueueTest.java
@@ -7,17 +7,37 @@ import java.util.Scanner;
 public class Ex04QueueTest {
     public static void main(String[] args) {
         Notice notice = new Notice();
-
-        notice.performNotice(notice.entrance(), "하하하");
-
+        while(notice.getNum() != 4) {
+            notice.entrance();
+            switch (notice.getNum()) {
+                case 1:
+                    notice.registerNotice();
+                    break;
+                case 2:
+                    notice.listNotice();
+                    break;
+                case 3:
+                    notice.deleteNotice();
+                    break;
+                case 4:
+                    notice.setNum(4);
+                    break;
+            }
+        }
+        System.out.println("프로그램 종료");
     }
 }
 
 class Notice {
     private Queue<String> notices;
+    private int num;
 
     public Notice() {
         this.notices = new LinkedList<>();
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 
     public int entrance() {
@@ -27,33 +47,47 @@ class Notice {
         System.out.println("2. 공지사항 목록 보기");
         System.out.println("3. 가장 오래된 공지사항 삭제");
         System.out.println("4. 나가기");
-        int num = scanner.nextInt() - 1;
+        num = scanner.nextInt();
         return num;
     }
 
+    public int getNum() {
+        return num;
+    }
 
-    public void performNotice(int num, String notice) {
-        switch(num) {
-            case 0:
-                registerNotice(notice);
-                break;
-            case 1:
+    public void registerNotice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---공지사항 등록---");
+        System.out.println("새로운 공지사항을 입력하세요.");
+        String str = scanner.nextLine();
+        notices.offer(str);
+        System.out.println("공지가 등록되었습니다.");
+    }
 
-            default:
-                System.out.println("유효성 없음");
+    public void listNotice() {
+        System.out.println("---공지사항 목록---");
+        for (String notice : notices) {
+            System.out.println(notice);
         }
     }
 
-    public void registerNotice(String notice) {
-        notices.add(notice);
-        System.out.println("공지가 등록되었습니다.");
-        System.out.println(notice);
+    public void deleteNotice() {
+        System.out.println("---공지사항 삭제---");
+        System.out.println("---가장 오래된 공지사항을 삭제합니다---");
+        notices.poll();
+        for (String notice : notices) {
+            System.out.println(notice);
+        }
     }
-
 }
 
 
-enum Function {REGISTER, LIST, DELETE,EXIT;}
+enum Function {
+    REGISTER,
+    LIST,
+    DELETE,
+    EXIT;
+}
 
 /*
 문제 1.
