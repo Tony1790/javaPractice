83dea318a05f2ecc327491dc2056d7ce0af70e86
Tony1790 <tony45081790@gmail.com>
2023-03-20 19:27:03 +0900

    Java practice at home

--- src/com/test/java/ch18/Ex14ComparatorTest.java
+++ src/com/test/java/ch18/Ex14ComparatorTest.java
@@ -1,8 +1,71 @@
 package com.test.java.ch18;
 
+import java.util.*;
+
 public class Ex14ComparatorTest {
+    public static void main(String[] args) {
+        PriorityQueue<Booka> pq = new PriorityQueue<>();
+        Collections.addAll(
+                pq,
+                new Booka("Kill the MockingBird", 35000),
+                new Booka("Harry potter", 50000),
+                new Booka("Lord of Ring", 28000),
+                new Booka("Me before You", 15000),
+                new Booka("Temptation", 59000)
+        );
+
+
+
+
+
+        /*List<Booka> list = new ArrayList<>();
+        list.addAll(pq);
+        Collections.sort(list, new Comparator<Booka>() {
+            @Override
+            public int compare(Booka b1, Booka b2) {
+                int result = Integer.valueOf(b1.getPrice()).compareTo(b2.getPrice()) * -1;
+                if(result == 0)
+                    result = b1.getTitle().compareTo(b2.getTitle());
+                return result;
+            }
+        });
+        System.out.println("높은 가격순 정렬");
+        System.out.println(list);*/
+
+
+    }
 }
 
+class Booka implements Comparable<Booka> {
+    private String title;
+    private int price;
+
+    public Booka(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public int compareTo(Booka book) {
+        int result = Integer.valueOf(price).compareTo(book.getPrice()) * -1;
+        if(result == 0)
+            result = title.compareTo(book.getTitle()) * -1;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "제목: " + title + ", 가격: " + price + "\n";
+    }
+}
 
 
 /*
